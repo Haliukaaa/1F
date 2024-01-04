4646f3f9f1960d1e9313d40f618d4a039225158e
Haliukaaa <@haliuka.aqua@gmail.com>
2024-01-04 18:49:12 +0800

    todo updated

--- week 8/day3.js
+++ week 8/day3.js
@@ -1,2 +1,24 @@
-let object = { a: 1, c: 3, b: 5, d: 4 };
-let answer = [];
+let object = { a: 1, d: 4, c: 3, b: 5 };
+
+const sortInAscendingOrder = (obj) => {
+  let sortedArray = Object.entries(obj).sort();
+  return Object.fromEntries(sortedArray);
+};
+
+console.log(sortInAscendingOrder(object));
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array);
+let reversedArray = [];
+
+const reverseArray = (arr) => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversedArray.push(arr[i]);
+  }
+  return reversedArray;
+};
+console.log(reverseArray(array));
+
+let string = array.toString("");
+let split = string.replaceAll(",", "");
+console.log(split);
---
+++ week 8/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+import { v4 as uuidv4 } from "uuid";
+
+let bold = uui();
+
+console.log(boldm, "hello");
