a30a43de9373bc07746960d8ccd8e0a711755455
Haliukaaa <haliuka.aqua@gmail.com>
2024-01-17 18:49:30 +0800

    began portfolio project

---
+++ Projects/Portfolio/portfolio/src/components/layout/index.js
@@ -0,0 +1,7 @@
+export * from '@/components/layout/Header';
+export * from '@/components/layout/About';
+export * from '@/components/layout/Contact';
+export * from '@/components/layout/Experience';
+export * from '@/components/layout/Footer';
+export * from '@/components/layout/Profile';
+export * from '@/components/layout/Skills';
---
+++ Projects/Portfolio/portfolio/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google';
+import { Header, About, Contact, Experience, Footer, Profile, Skills } from '@/components/layout/index';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <></>
+  );
+}
